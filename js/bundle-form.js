30433e562040ca36faec934089c1258c3a2db80b
Matt Difford <matt@mrsavvy.co.uk>
2020-01-21 12:07:03 +0000

    Optimise JS

---
+++ js/bundle-form.js
@@ -0,0 +1,9 @@
+---
+---
+
+{% include_relative  src/jquery.min.js %}
+{% include_relative  src/script.js %}
+{% include_relative  src/lazysizes.min.js %}
+{% include_relative  src/parsley.min.js %}
+{% include_relative  src/slick.min.js %}
+{% include_relative  src/form.js %}
---
+++ js/bundle.js
@@ -0,0 +1,7 @@
+---
+---
+
+{% include_relative  src/jquery.min.js %}
+{% include_relative  src/slick.min.js %}
+{% include_relative  src/lazysizes.min.js %}
+{% include_relative  src/script.js %}
