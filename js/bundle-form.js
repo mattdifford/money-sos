4001f8dde67c44788efb025be544188c91f42938
Matt Difford <matt@mrsavvy.co.uk>
2020-01-22 14:40:04 +0000

    JS changes

--- js/bundle-form.js
+++ js/bundle-form.js
@@ -1,9 +1,9 @@
 ---
 ---
 
-{% include_relative  src/jquery.min.js %}
 {% include_relative  src/script.js %}
 {% include_relative  src/lazysizes.min.js %}
 {% include_relative  src/parsley.min.js %}
 {% include_relative  src/slick.min.js %}
 {% include_relative  src/form.js %}
+{% include_relative  src/field_date.js %}
--- js/bundle.js
+++ js/bundle.js
@@ -1,7 +1,6 @@
 ---
 ---
 
-{% include_relative  src/jquery.min.js %}
 {% include_relative  src/slick.min.js %}
 {% include_relative  src/lazysizes.min.js %}
 {% include_relative  src/script.js %}
