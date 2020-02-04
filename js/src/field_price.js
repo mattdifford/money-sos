072994d878dbffcff7b2ba2fea80e0acd9a4a8d5
Matt Difford <matt@mrsavvy.co.uk>
2020-02-04 15:08:01 +0000

    Featured strip, price field js

--- js/src/field_price.js
+++ js/src/field_price.js
@@ -1,6 +1,6 @@
 $(".field__input--price").on("keyup", function () {
     var n = parseInt($(this).val().replace(/\D/g, ''), 10);
-    if (!n.toLocaleString().isNaN()) {
+    if (n.toLocaleString() != "NaN") {
         $(this).val(n.toLocaleString());
     }
 });
