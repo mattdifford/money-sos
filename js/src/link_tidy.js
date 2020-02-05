ab9fa1027afe73d013a738e0144d02401c1dfb81
Matt Difford <matt@mrsavvy.co.uk>
2020-02-05 16:11:59 +0000

    Better link tidy

--- js/src/link_tidy.js
+++ js/src/link_tidy.js
@@ -1,6 +1,7 @@
 var current_url = window.location.protocol + '//' + window.location.hostname;
 $('a').each(function(){
-    if ($(this).attr("href").indexOf(current_url) == -1){
+    var href = $(this).attr("href");
+    if (href.charAt(0) != '/' && href.substr(0, 7) != 'mailto:' && href.indexOf(current_url) == -1){
         $(this).attr("rel", "external noopener")
         $(this).attr("target", "_blank")
     }
