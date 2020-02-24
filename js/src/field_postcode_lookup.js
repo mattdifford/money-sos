7a4e9f65313f572bf4008b9391df036aba9b27c0
Matt Difford <matt@mrsavvy.co.uk>
2020-02-24 15:59:25 +0000

    Broadband journey

--- js/src/field_postcode_lookup.js
+++ js/src/field_postcode_lookup.js
@@ -1,39 +1,121 @@
-/**Array indices for getAddress API response**/
-const LINE_1_INDEX = 0;
-const LINE_2_INDEX = 1;
-const TOWNCITY_INDEX = 5;
-const COUNTY_INDEX = 6;
-$('.field__lookup-button button').on("click", function () {
-    var value = $(this).parents('.field__input-wrap').find('.field__input').val();
-    var parent_form = $(this).parents('.form');
-    parent_form.addClass('form--loading');
-    $.get("https://api.getAddress.io/find/" + value.replace(/\s/g, ''), { 'api-key': "E6M9hcmutUGNSzoxzNdpRg22683", sort: true }, function (data, status) {
-        var address_details = [];
-        data.addresses.forEach(function (item) {
-            address_details.push(item.split(","));
-        });
-        var html = '';
-        html += '<div class="field__postcode-lookup-list postcode-lookup-list">';
-        address_details.forEach(function (item) {
-            var formatted_add = item.filter(function (el) {
-                return el !== null && el !== ' ' && el !== '';
-            });
-            html += '<a class="postcode-lookup-list__item" href="#" data-line-1="' + item[LINE_1_INDEX] + '" data-line-2="' + item[LINE_2_INDEX] + '" data-town="' + item[TOWNCITY_INDEX] + '" data-county="' + item[COUNTY_INDEX] + '">' + formatted_add.join(', ') + '</a>';
-        });
-        html += '</div>';
-        $('#{{results_id}}').append(html);
-        parent_form.removeClass('form--loading');
-    });
-});
+const api_key = "PCWNW-9FSGT-69N88-H35WL";
 
-$("body").on("click", ".postcode-lookup-list__item", function (e) {
-    var address_fields = $(this).parents('.form__field').find('.form__field');
-    $(this).parents('.field__input-wrap').find('.field__lookup-button').remove();
+function address_search(input_element, address_results, status_element, page) {
+    $(address_results).empty();
+
+    var page = page || 0;
+
+    var address = $(input_element)
+        .val()
+        .trim();
+
+    if (address != "") {
+        if (page == 0) $(address_results).empty();
+
+        var loading_html = $("<div></div>", {
+            id: "address_loading",
+            text: "Searching addresses...",
+        });
+        $(input_element).after(loading_html);
+
+        $(status_element).text("Searching addresses");
+
+        var country_code = "GB";
+
+        var address_url =
+            "https://ws.postcoder.com/pcw/" +
+            api_key +
+            "/address/" +
+            country_code +
+            "/" +
+            encodeURIComponent(address) +
+            "?lines=2&page=" +
+            page;
+
+        // Call the API
+        $.ajax({
+            url: address_url,
+        }).done(function (data) {
+            $("#address_loading").remove();
+            // For only one result, simply populate the fields, rather than asking the user to select from list
+            if (data.length == 1) {
+                select_address(data[0], address_results, status_element);
+                $(status_element).text('"' + data[0].summaryline + '" selected, address fields below populated');
+            } else if (data.length > 1) {
+                var select_element = $(
+                    "<select name='address_select' id='address_select_element' class='field__input field__input--select postcode-address-select'></select>"
+                );
+                var select_label = $(
+                    "<label for='address_select' class='field__label'>Choose an address</label>"
+                );
+
+                $(address_results)
+                    .append(select_label)
+                    .append(select_element);
+
+                $("#address_select_element").on("change", function (event) {
+                    if (event.target.value === "moreValues") {
+                        // Here we handle a request for more addresses
+                        // (if more than 100 were returned from the search)
+                        address_search(input_element, address_results, status_element, data[data.length - 1].nextpage);
+                    } else {
+                        if (typeof gtag === 'function') {
+                            gtag('event', $(input_element).attr("name"), { 'event_category': 'Form interaction', 'event_label': "Address select", });
+                        }
+                        $('.field--disabled').removeClass('field--disabled');
+                        select_address(data[event.target.value], address_results, status_element);
+                    }
+                });
+
+                for (var i = 0; i < data.length; i++) {
+                    if (i === 0) {
+                        var address_option = $("<option value='null'>Select an address...</option>");
+                        $("#address_select_element").append(address_option);
+                    }
+                    var address_option = $("<option value='" + i + "'>" + data[i].summaryline + "</option>");
+                    $("#address_select_element").append(address_option);
+                }
+                var last_index = data.length - 1;
+                if (data[last_index].morevalues) {
+                    var show_more_option = $("<option value='moreValues'>" + data[last_index].totalresults + " addresses found, click to show next 100</option>");
+                    $("#address_select_element").append(show_more_option);
+                } else {
+                    $(status_element).text(data.length + " addresses found");
+                }
+            } else {
+                $(address_results).text("No addresses found");
+                $(status_element).text("No addresses found");
+            }
+        }).fail(function (error) {
+            loading_html.remove();
+            $(status_element).text("Error occurred");
+
+            $(address_results).text("Error occurred");
+        });
+    } else {
+        // Could show an "Address search term is required" message here
+    }
+}
+
+function select_address(address, address_results, status_element) {
+    
+    if (address != null) {
+        $(status_element).text('"' + address.summaryline + '" selected, address fields below populated');
+        // Populate fields
+        $("input[name='address']").val(address.addressline1);
+        $("input[name='address2']").val(address.addressline2);
+        $("input[name='city']").val(address.posttown);
+        $("input[name='county']").val(address.posttown);
+        $("input[name='zip_code']").val(address.postcode);
+    } else {
+        $("input[name='address']").val("");
+        $("input[name='address2']").val("");
+        $("input[name='county']").val("");
+        $("input[name='zip_code']").val("");
+    }
+}
+
+$('.field__lookup-button button').on("click", function (e) {
     e.preventDefault();
-    $('#{{results_id}}').html('');
-    address_fields.show();
-    $('input[name="{{-include.slug-}}-line1"]').val($(this).attr("data-line-1"));
-    $('input[name="{{-include.slug-}}-line2"]').val($(this).attr("data-line-2"));
-    $('input[name="{{-include.slug-}}-towncity"]').val($(this).attr("data-town"));
-    $('input[name="{{-include.slug-}}-county"]').val($(this).attr("data-county"));
+    address_search('.field__input--postcode', "#postcode_lookup_results", "#address_status");
 });
